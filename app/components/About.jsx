2e800ae9227bc1757dbca6d052804dd735967d44
Paoonline <paoonline@windowslive.com>
2018-09-09 19:05:23 +0700

    adding css webpack

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
     return(
         <div>
-            <h1 className="text-center">About</h1>
+            <h1 className="text-center page_title">About</h1>
             <p>This a weather react app</p>
             <p>Here are some of the tools I used</p>
             <ul>
